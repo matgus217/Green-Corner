7cd4fe4ea714b6edabebffe6863c03d15fd2bc8d
matgus217 <>
2024-01-30 09:06:23 +0000

    Add links and styles for footer in css

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
 
 def get_index(request):
     return render(request, 'pages/index.html')
+
+
+def get_menu(request):
+    return render(request, 'pages/menu.html')
+
+
+def get_about(request):
+    return render(request, 'pages/about.html')
