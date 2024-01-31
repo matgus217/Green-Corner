087aa27b1b4f04c8ac8ca4995749a6e416d32add
matgus217 <>
2024-01-31 14:47:21 +0000

    Change debug to false

--- greencorner/settings.py
+++ greencorner/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['greencorner.herokuapp.com', 'localhost', '8000-matgus217-green-corner-tz8i2maemj.us2.codeanyapp.com',
                  'greencorner-f66e9d809546.herokuapp.com']
